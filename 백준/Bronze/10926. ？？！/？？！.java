18de957926c096f28e8dc9c4e2ba2a2ec665e436
new-h2 <79350383+new-h2@users.noreply.github.com>
2023-04-01 04:50:37 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+        String s = "??!";
+        System.out.print(id+s);
+    }
+}
