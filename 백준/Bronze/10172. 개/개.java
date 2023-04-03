538d6d426423add6018b94bab7d40d315f17b85c
new-h2 <79350383+new-h2@users.noreply.github.com>
2023-04-03 09:59:53 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+        //큰따옴표와 백슬래시 앞에다 개행문자 넣기
+    }
+}
