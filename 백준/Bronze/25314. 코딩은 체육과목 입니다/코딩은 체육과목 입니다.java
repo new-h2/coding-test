d7bcc07528f0b04636078cc3217b6b560289a182
new-h2 <79350383+new-h2@users.noreply.github.com>
2023-04-10 12:30:01 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 124 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -7,13 +7,12 @@ public class Main{
         
         int num  = Integer.parseInt(br.readLine());
         num = num / 4;
-       
+       // num을 4로나누기 나온값만큼 long을 sb에 넣기
+       // String은 final이기때문에 계속 값추가해줘야함,추가자동생성x
+       // StringBuilder는 문자열값을 넣으면 추가로 생성되는 배열형태  
         StringBuilder sb = new StringBuilder();
-        // num을 4로나누기 나온값만큼 long출력
-            
         for(int i=0; i<num; i++){
-           // i * long만큼 넣기
-           sb.append("long ");
+           sb.append("long "); //공백 
          }
         sb.append("int"); //마지막에 int출력
         System.out.print(sb);
